264251bbb36617f5f890f49cb68b9d9c46190718
Nik Zap <nick@nijotz.com>
2023-02-01 08:58:25 -0800

    Fix nvm

--- shell/nvm.bash
+++ shell/nvm.bash
@@ -1,13 +1,13 @@
 # Specific to homebrew
-NVMSH=/usr/local/opt/nvm/nvm.sh
+NVMSH=$(brew --prefix)/opt/nvm/nvm.sh
 if [ -f $NVMSH ]; then
   nomad_log info "found nvm.sh; configuring"
+  nomad_echo_and_eval << EOF
+  export NVM_DIR=$HOME/.nvm
+  source $NVMSH
+EOF
 else
   nomad_log info "no nvm.sh found; skipping"
   exit 0
 fi
 
-nomad_echo_and_eval << EOF
-export NVM_DIR=$HOME/.nvm
-source $NVMSH
-EOF
