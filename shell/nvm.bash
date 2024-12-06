8c728451efd69f80b9046ea10c30aedf7910b464
Nick Tzaperas <nick@nijotz.com>
2024-12-05 22:25:58 -0800

    Handle new version of nvm

--- shell/nvm.bash
+++ shell/nvm.bash
@@ -1,18 +1,17 @@
 brew_prefix=$(brew --prefix 2>/dev/null)
 if [[ "$brew_prefix" ]]; then
-  NVMSH=$(brew --prefix)/opt/nvm/nvm.sh
+  export NVM_DIR="${brew_prefix}/opt/nvm"
 elif [[ -f ~/.nvm/nvm.sh ]]; then
-  NVMSH=~/.nvm/nvm.sh
+  export NVM_DIR="$HOME/.nvm"
 fi
+
+NVMSH=${NVM_DIR}/nvm.sh
 
 if [[ -f $NVMSH ]]; then
   nomad_log info "found nvm.sh; configuring"
   nomad_echo_and_eval << EOF
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 EOF
 else
   nomad_log info "no nvm.sh found; skipping"
-  exit 0
 fi
