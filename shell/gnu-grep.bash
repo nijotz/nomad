e3b29425ea320c0554ee62a6627da4b596530804
Nick Tzaperas <nick@nijotz.com>
2025-10-11 18:15:19 -0700

    Use GNU grep

---
+++ shell/gnu-grep.bash
@@ -0,0 +1,15 @@
+brew_prefix=$(brew --prefix 2>/dev/null)
+if [[ -z "$brew_prefix" ]]; then
+  nomad_log info "homebrew binary not found"
+  exit 0
+fi
+
+nomad_log info "found homebrew binary; configuring GNU grep"
+
+nomad_echo_and_eval <<EOF
+# I don't want to have to type 'ggrep'
+export PATH=$brew_prefix/opt/grep/libexec/gnubin:\$PATH
+
+# Need the man pages for the GNU grep
+export MANPATH=$brew_prefix/opt/grep/libexec/gnuman:\$MANPATH
+EOF
